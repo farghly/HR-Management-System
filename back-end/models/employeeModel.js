7afb5a1e33fb4a650407cf6e82d4fb4877b796b7
Ahmed Shawareb <js.dev.ahmedshawareb@gmail.com>
2022-11-05 03:30:07 +0200

    modify serveral files to increase proffitionalilty

--- back-end/models/employeeModel.js
+++ back-end/models/employeeModel.js
@@ -1,53 +1,80 @@
 // Employee model
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
 const employeeSchema = mongoose.Schema({
-    firstName:{
-        type:String,
-        required:true,
-    },
-    lastName:{
-        type:String,
-        required:true,
-    },
-    // Department:{
-    //     type:mongoose.Schema.Types.ObjectId,
-    //     ref:"Department",
-    //     required:true,
-    // },
-    // Designation:{
-    //     type:mongoose.Schema.Types.ObjectId,
-    //     ref:"Designation",
-    //     required:true,
-    // },
-    role:{
-        type:String,
-        required:true,
-    },
-    Gender:{
-        type:String,
-        required:true,
-    },
-    // NID:{
-    //     type:Number,
-    //     required:true,
-    // },
-    // DateOfBirth:{
-    //     type:Date,
-    //     required:true,
-    // },
-    // DateOfJoin:{
-    //   type:Date,
-    //   required:true,
-    // },
-    // DateOfLeave:{
-    //     type:Date,
-    //     required:true,
-    // },
-    email:{
-        type:String,
-        required:true
-    }
-})
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  department: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Department",
+    required: true,
+  },
+  //   Designation:{
+  //       type:mongoose.Schema.Types.ObjectId,
+  //       ref:"Designation",
+  //       required:true,
+  //   },
+  role: {
+    type: String,
+    required: true,
+    default: "employee",
+    enum: ["admin", "manager", "HR", "employee", "Senior", "junior"],
+  },
+  Gender: {
+    type: String,
+    required: true,
+    enum: ["male", "female"],
+  },
+  NID: {
+    type: Number,
+    required: true,
+    min: 14,
+    max: 14,
+  },
+  DateOfBirth: {
+    type: Date,
+    required: true,
+  },
+  DateOfJoin: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+  },
+  DateOfLeave: {
+    type: Date,
+  },
+  email: {
+    type: String,
+    required: [true, "User must has an email"],
+    unique: true,
+    trim: true,
+    lowercase: true,
+    validate: [validator.isEmail, "Email must be in a right form"],
+  },
+  password: {
+    type: String,
+    required: [true, "User must have a password"],
+    minlength: 8,
+    select: false,
+  },
+  confirmPassword: {
+    type: String,
+    required: [true, "User must confirm password"],
+    select: false,
+  },
+  passwordChangedAt: Date,
 
-module.exports = mongoose.model('Employee',employeeSchema)
+  passwordResetToken: String,
+  passwordResetExpires: Date,
+});
+
+module.exports = mongoose.model("Employee", employeeSchema);
--- server.js
+++ server.js
@@ -1,11 +1,37 @@
+const rateLimiter = require("express-rate-limit");
+
+const helmet = require("helmet");
+
+const xssClean = require("xss-clean");
+
+const mongoSanitize = require("express-mongo-sanitize");
+
+const ErrorController = require("./back-end/controller/errorController");
+
+const AppError = require("./back-end/utils/appError");
 const dotenv = require("dotenv");
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const employee = require("./back-end/routes/employeeRoute");
+const employeeRouter = require("./back-end/routes/employeeRoutes");
+// const employee = require("./back-end/routes/employeeRoute");
+
 const department = require("./back-end/routes/departmentRoute");
 const designation = require("./back-end/routes/designationRoute");
 
+const limiter = rateLimiter({
+  max: 100,
+  windowsMS: 60 * 60 * 1000,
+  message: "Too many requests , please try again after an hour",
+});
+app.use(helmet());
+app.use("/api", limiter);
+app.use(mongoSanitize());
+app.use(xssClean());
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 app.use(express.json());
 // get configuration file
 dotenv.config({ path: "./back-end/config.env" });
@@ -16,9 +42,9 @@ const DB = process.env.DATABASE.replace(
 );
 const PORT = process.env.PORT;
 
-
 // register routes
-app.use(employee);
+// app.use(employee);
+app.use("/api/v1/employees", employeeRouter);
 app.use(department);
 app.use(designation);
 
@@ -26,10 +52,19 @@ app.get("/", (req, res) => {
   res.send("Hello from the home page");
 });
 
+app.all("*", (req, res, next) => {
+  //   res.status(404).json({
+  //     status: 'failed',
+  //     msg: `Error to reach ${req.originalUrl} on this server`,
+  //   });
+
+  next(new AppError("Error", 404));
+});
 mongoose.connect(DB, {}).then((connect) => {
   console.log("connect to database successfully");
 });
 
+app.use(ErrorController);
 app.listen(PORT, () => {
   console.log(`App running on port http://localhost:${PORT}`);
 });
