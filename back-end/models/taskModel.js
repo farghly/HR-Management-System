3e812239d9a68b172d5c857d65ae99b7b9d2a69d
Ahmed Shawareb <js.dev.ahmedshawareb@gmail.com>
2022-11-05 04:23:10 +0200

    create task schema

---
+++ back-end/models/taskModel.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "The task must have a name"],
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  summery: {
+    type: String,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: [true, "The task must have a description"],
+  },
+  project: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Project",
+    // required :[true , "The Task must belongs to a project "]
+  },
+  employee: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Employee",
+    },
+  ],
+  startDate: {
+    type: Date,
+    default: Date.now(),
+  },
+  endDate: {
+    type: Date,
+    required: [true, "Task must have end date"],
+  },
+  status: {
+    type: String,
+    enum: ["Done", "Is Going", "Delayed"],
+    required: [true, "The task must have a status"],
+  },
+  importance: {
+    type: String,
+    default: "Normal",
+    enum: ["Normal ", "Very Important", "Urgent", "Can be done later"],
+  },
+});
+
+const Task = mongoose.model("Task", taskSchema);
+
+module.exports = Task;
