557b53b64aef9a6fb89282bb2dbd4f5029006490
farghly <eng_farghlyahmed@yahoo.com>
2022-11-10 09:01:25 +0200

    update department actions

--- src/actions/department.js
+++ src/actions/department.js
@@ -2,13 +2,25 @@ import { FETCH_ALL} from '../constants/actionTypes';
 
 import * as api from '../api/index.js';
 
-export const getPosts = () => async (dispatch) => {
+export const getDepartments = () => async (dispatch) => {
   try {
-    const { data } = await api.fetchPosts();
+    const { data } = await api.fetchDepartments();
 
     dispatch({ type: FETCH_ALL, payload: data });
   } catch (error) {
     console.log(error.message);
   }
 };
+
+
+export const deleteDepartment = (id) => async (dispatch) => {
+  try {
+    await api.deleteDepartment(id);
+
+    dispatch({ type: DELETE, payload: id });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
 
--- src/api/index.js
+++ src/api/index.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 const departmentUrl = 'http://localhost:9090/api/v1/department';
 
-export const fetchDepartment = () => axios.get(departmentUrl);
+export const fetchDepartments = () => axios.get(departmentUrl);
 export const deleteDepartment = (id) => axios.delete(`${departmentUrl}/${id}`);
