bd4832ffbeca88d2944529b13818a083bbc554a4
Ahmed Shawareb <js.dev.ahmedshawareb@gmail.com>
2022-11-10 20:54:33 +0200

    fix login problem

--- back-end/models/employeeModel.js
+++ back-end/models/employeeModel.js
@@ -97,6 +97,13 @@ const employeeSchema = new mongoose.Schema({
   passwordResetToken: String,
   passwordResetExpires: Date,
 });
+
+employeeSchema.methods.comparePassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 employeeSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 12);
