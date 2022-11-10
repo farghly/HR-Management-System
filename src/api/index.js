4446348cb412ab5ba51572c68baf44903f3984cd
farghly <eng_farghlyahmed@yahoo.com>
2022-11-10 07:59:37 +0200

    Add department API URL

---
+++ src/api/index.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+const departmentUrl = 'http://localhost:9090/api/v1/department';
+
+export const fetchDepartment = () => axios.get(departmentUrl);
+export const deleteDepartment = (id) => axios.delete(`${departmentUrl}/${id}`);
