71c23d6eb09b9d4dd74fce6026138d738310c047
M-Sedky <Muhammad.Sedky.eg@gmail.com>
2022-11-17 13:14:07 +0200

    create addtasks page and some modifications

--- src/pages/tasks/addtask.js
+++ src/pages/tasks/addtask.js
@@ -1,34 +1,72 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { createTask } from '../../api/tasksAPI';
+import FormInput from '../../components/form-input/FormInput.component';
 import './task.css'
-function AddTask() {
-    return ( 
-      <>
-      <Link to="../tasks" class="btn btn-primary my-3 task-list">Tasks List</Link>
-   <h3>Add New Task</h3>
-   <form action="" class="d-flex gap-4 my-5">
-     <div class="data d-flex flex-column gap-2">
-       <label for="tName">Task Name</label>
-       <input type="text" id="tName" autocomplete="off" />
-     </div>
-     <div class="data d-flex flex-column gap-2">
-       <label for="description">Description</label>
-       <textarea name="" id="description" rows="4"></textarea>
-     </div>
-     <div class="data d-flex flex-column gap-2">
-       <label for="summary">Summary</label>
-       <textarea name="" id="summary" rows="4"></textarea>
-     </div>
-     <div class="data d-flex flex-column gap-2">
-       <label for="employee">Employee</label>
-       <select id="employee" name="employee" class="select">
-         <option value="one">One</option>
-         <option value="two">Two</option>
-       </select>
-     </div>
-     <input class="btn btn-success" type="submit" value="Save" />
-   </form>
 
-   </>
-     );
+const defaultFormData = {
+  name: '',
+  createdAt: '',
+  summary: '',
+  description: '',
+  project: '',
+  employee: '',
+  startDate: '',
+  endDate: '',
+  status: '',
+  importance: ''
+}
+
+function AddTask() {
+
+  const [formData, setFormData] = useState(defaultFormData);
+
+  const { name, summary, description, project, employee, startDate, endDate, status, createdAt, importance } = formData;
+
+  const changeHandler = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+    console.log(formData);
+  }
+
+  const resetFormData = () => {
+    setFormData(defaultFormData);
+  };
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    createTask(formData);
+    resetFormData();
+  };
+
+  return (
+    <>
+      <Link to="../tasks" className="btn btn-primary my-3 task-list">Tasks List</Link>
+      <h3>Add New Task</h3>
+      <form action="" className="d-flex gap-4 my-5" onSubmit={submitHandler}>
+        <div className="data d-flex flex-column gap-2">
+          <label for="tName">Task Name</label>
+          <FormInput type="text" id="tName" autocomplete="off" value={name} onChange={changeHandler} />
+        </div>
+        <div className="data d-flex flex-column gap-2">
+          <label for="description">Description</label>
+          <textarea className='border border-0' name="" id="description" rows="4" vlaue={description} onChange={changeHandler}></textarea>
+        </div>
+        <div className="data d-flex flex-column gap-2">
+          <label for="summary">Summary</label>
+          <textarea className='border border-0' name="" id="summary" rows="4" value={summary} onChange={changeHandler}></textarea>
+        </div>
+        <div className="data d-flex flex-column gap-2">
+          <label for="employee">Employee</label>
+          <select id="employee" name="employee" className="select" onChange={changeHandler}>
+            <option value="one">One</option>
+            <option value="two">Two</option>
+          </select>
+        </div>
+        <input className="btn btn-success" type="submit" value="Save" />
+      </form>
+
+    </>
+  );
 }
 export default AddTask;
